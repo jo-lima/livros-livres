9c21bd2e140e11726bb8570bb1588315bd455443
jo-lima <joaovictorlimachaves@gmail.com>
2025-09-19 20:02:14 -0300

    Created registers folder, added user class

---
+++ private/registers/usuarios/usuario.java
@@ -0,0 +1,17 @@
+public class Usuario{
+    String cpf;
+    String nome;
+    String email;
+    String senha;
+    String endereco;
+    String telefone;
+
+    public Usuario(String cpf, String nome, String email, String senha, String endereco, String telefone){
+        this.cpf      = cpf;
+        this.nome     = nome;
+        this.email    = email;
+        this.senha    = senha
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+}

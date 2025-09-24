a330c759847684f7d32961bc326e0b011404ed8f
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-09-24 17:38:20 -0300

    Added springboot to the project

---
+++ Server/src/main/java/com/livros_livres/Server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.livros_livres.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}

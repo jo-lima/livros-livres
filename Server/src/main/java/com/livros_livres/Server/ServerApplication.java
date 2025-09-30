901ecb0facf8dd55a131d1c3516e64c6731b77c4
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-09-30 15:59:20 -0300

    Removed springboot excluding datasourceautoconfig

--- Server/src/main/java/com/livros_livres/Server/ServerApplication.java
+++ Server/src/main/java/com/livros_livres/Server/ServerApplication.java
@@ -2,9 +2,8 @@ package com.livros_livres.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class ServerApplication {
 
 	public static void main(String[] args) {

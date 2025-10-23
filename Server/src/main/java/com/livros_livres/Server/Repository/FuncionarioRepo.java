c4726dc6be0c08f7de89ea80fa29dffd303c05e5
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-10-23 14:55:30 -0300

    Added funcionario methods to test out their authentication

--- Server/src/main/java/com/livros_livres/Server/Repository/FuncionarioRepo.java
+++ Server/src/main/java/com/livros_livres/Server/Repository/FuncionarioRepo.java
@@ -1,8 +1,11 @@
 package com.livros_livres.Server.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.livros_livres.Server.Registers.usuarios.Cliente;
 import com.livros_livres.Server.Registers.usuarios.Funcionario;
 
 // Repository that manages database connection.
@@ -10,5 +13,5 @@ import com.livros_livres.Server.Registers.usuarios.Funcionario;
 // TODO: List all methods this class has.
 @Repository
 public interface FuncionarioRepo extends JpaRepository<Funcionario, Integer> {
-
+    Optional<Funcionario> findByMatriculaIgnoreCase(String matricula);
 }
--- Server/src/main/java/com/livros_livres/Server/services/FuncionarioService.java
+++ Server/src/main/java/com/livros_livres/Server/services/FuncionarioService.java
@@ -1,5 +1,7 @@
 package com.livros_livres.Server.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,18 @@ public class FuncionarioService {
 
     public Funcionario salvarFuncionario( Funcionario funcionarioData ){
         return funcionarioRepo.save(funcionarioData);
+    }
+
+    public Funcionario buscaFuncionarioMatricula(String matricula) {
+        Optional<Funcionario> funcionario;
+
+        funcionario = funcionarioRepo.findByMatriculaIgnoreCase(matricula);
+
+        if(funcionario.isEmpty()) {
+            return null;
+        }
+
+        return funcionario.get();
     }
 
 }

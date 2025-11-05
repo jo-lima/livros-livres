8b9fbbf7cb505faae9249decebe914635fae3ff4
jo-lima <joaovictorlimachaves@gmail.com>
2025-11-05 18:14:48 -0300

    Request feito, mudado gitignore

--- public/scripts/inicio/inicio.js
+++ public/scripts/inicio/inicio.js
@@ -1,14 +1,9 @@
-const json = {
-    nome:      "Luca Maia",
-    descricao: "feliz :D",
-    citacao:   "kpwqon dfbbfe",
-    atio:      true
-}
+
 
 // Elementos
 const authorNameElement = document.querySelector(".author-name")
 const authorDescriptionElement = document.querySelector('.author-biography')
-const authorQuoteElement = document.querySelector('.author-detail__descriptione')
+const authorQuoteElement = document.querySelector('.author-detail__description')
 
 function renderAuthor(json){
     authorNameElement.textContent = json.nome
@@ -17,4 +12,26 @@ function renderAuthor(json){
 }
 
 
-renderAuthor(json)
+
+const params =  new URLSearchParams(document.location.search);
+let id = params.get("id");
+
+console.log(id)
+
+// renderAuthor(json)
+
+headers = {
+    'Access-Control-Allow-Origin' : '*',
+    // 'Access-Control-Allow-Credentials': 'true'
+}
+
+async function a(){
+    const response = await fetch(`http://localhost:6969/autor/${id}/busca` , {headers: headers, method: 'GET'})
+    const json = await response.json()
+    console.log(json.body)
+    renderAuthor(json.body)
+}
+
+
+const json = a()
+console.log(json)

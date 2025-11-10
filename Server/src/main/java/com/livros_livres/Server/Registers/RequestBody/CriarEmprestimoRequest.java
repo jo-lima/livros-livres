58bff62647f8c8041a76e9d8750debc5047b8481
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-11-10 15:45:06 -0300

    Added new field to criaremprestimorequest

--- Server/src/main/java/com/livros_livres/Server/Registers/RequestBody/CriarEmprestimoRequest.java
+++ Server/src/main/java/com/livros_livres/Server/Registers/RequestBody/CriarEmprestimoRequest.java
@@ -1,5 +1,7 @@
 package com.livros_livres.Server.Registers.RequestBody;
 
+import java.time.LocalDate;
+
 import io.micrometer.common.lang.NonNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,4 +15,6 @@ public class CriarEmprestimoRequest {
     @NonNull
     private Integer clienteId;
 
+    @NonNull
+    private LocalDate dataPrevistaDevolucao;
 }

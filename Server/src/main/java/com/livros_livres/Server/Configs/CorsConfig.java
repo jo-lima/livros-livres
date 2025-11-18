1a5ca8ad44aae02acc4bd1f69644b2ab6d3d62c3
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-11-18 17:11:16 -0300

    Trying stuff

--- Server/src/main/java/com/livros_livres/Server/Configs/CorsConfig.java
+++ Server/src/main/java/com/livros_livres/Server/Configs/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all paths
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://152.250.104.13:5500") // Replace with your allowed origins
+                        .allowedOriginPatterns("http://127.0.0.1:5500", "http://localhost:5500", "http://152.250.104.13:5500") // Replace with your allowed origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                         .allowedHeaders("*") // Allowed headers
-                        .allowCredentials(true) // Allow sending cookies/authorization headers
+                        // .allowCredentials(true) - Remove this line
                         .maxAge(3600); // Max age for pre-flight requests (in seconds)
             }
         };
